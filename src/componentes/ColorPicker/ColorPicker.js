788256e8a47b87c22720c8806293e6cf477f32e4
DevGonza <gonzalo.sanchez.s@gmail.com>
2022-06-18 11:55:11 -0300

    agrego gardient por defecto

--- src/componentes/ColorPicker/ColorPicker.js
+++ src/componentes/ColorPicker/ColorPicker.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react'
 import './ColorPicker.css'
 
 export const ColorPicker = () => {
-  const [colorSelect, setColorSelect] = useState ('');
+  const [colorSelect, setColorSelect] = useState ('radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)');
 
 
   return (
-    <div className='container ' style={{backgroundColor:colorSelect}}>
+    <div className='container ' style={{background:colorSelect}}>
       <div className="formColor">
         <p>Color Picker</p>
         <div className='separador'>
           <label htmlFor="exampleColorInput" className="form-label"></label>
           <input type="color" className="form-control form-control-color" id="exampleColorInput" value={colorSelect} onChange={(e)=>setColorSelect(e.target.value)} title="Choose your color" />
-          <input className="form-input-text" type="text" placeholder="Color seleccionado"  value={colorSelect} onChange={(e) => setColorSelect(e.target.value)} aria-label="default input example" disabled />
+          <input className="form-input-text" type="text" placeholder="Color seleccionado"  value={colorSelect === 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)' ? '' :  colorSelect} onChange={(e) => setColorSelect(e.target.value)} aria-label="default input example" />
         </div>
       </div>
     </div>
